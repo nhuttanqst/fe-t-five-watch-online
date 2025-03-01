4aad0afe7b9c1c466263914c993bd5973367dc92
Ngo Nhut Tan <ngonhattan.sd1@gmail.com>
2025-03-01 16:50:13 +0700

    done banner & productcategory

--- src/pages/client/Home.jsx
+++ src/pages/client/Home.jsx
@@ -1,5 +1,29 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import banner1 from "../../assets/banner1.png";
+import banner2 from "../../assets/banner2.png";
+import ProductCategories from "../../components/ProductCategory/ProductCategories";
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <div className="container mt-4 mb-20">
+        <Carousel showThumbs={false} showStatus={false} infiniteLoop autoPlay>
+          <div>
+            <img src={banner1} alt="banner 1" />
+          </div>
+          <div>
+            <img src={banner2} alt="banner 2" />
+          </div>
+          <div>
+            <img src={banner1} alt="banner 3" />
+          </div>
+        </Carousel>
+
+        <ProductCategories />
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
