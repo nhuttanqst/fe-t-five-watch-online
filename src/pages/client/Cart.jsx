777fdf4d58cfda34520349e85c223646a48610ad
Ngo Nhut Tan <ngonhattan.sd1@gmail.com>
2025-03-04 21:52:01 +0700

    update cart.jsx

--- src/pages/client/Cart.jsx
+++ src/pages/client/Cart.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import product1 from "../../assets/product1.png";
 import product2 from "../../assets/product2.png";
 import product3 from "../../assets/product3.png";
@@ -9,28 +8,28 @@ import creditcard from "../../assets/creditcard.png";
 import { DeleteOutlined } from "@ant-design/icons";
 import { Col, Row } from "antd";
 
-const CartPage = () => {
-  const [items, setItems] = useState([
-    {
-      name: "Doxa Noble D173TCM",
-      price: 49240000,
-      quantity: 1,
-      image: product1,
-    },
-    {
-      name: "KOI Sheen K004.53.644.53.10.05",
-      price: 7520000,
-      quantity: 2,
-      image: product2,
-    },
-    {
-      name: "Titoni Doi K-DB541",
-      price: 85510000,
-      quantity: 1,
-      image: product3,
-    },
-  ]);
+const items = [
+  {
+    name: "Doxa Noble D173TCM",
+    price: 49240000,
+    quantity: 1,
+    image: product1,
+  },
+  {
+    name: "KOI Sheen K004.53.644.53.10.05",
+    price: 7520000,
+    quantity: 2,
+    image: product2,
+  },
+  {
+    name: "Titoni Doi K-DB541",
+    price: 85510000,
+    quantity: 1,
+    image: product3,
+  },
+];
 
+const CartPage = () => {
   const calculateTotal = () => {
     return items.reduce((total, item) => total + item.price * item.quantity, 0);
   };
@@ -79,13 +78,15 @@ const CartPage = () => {
             </div>
           ))}
         </div>
-        
+
         <div className="flex items-center py-2 cursor-pointer text-black m-0 gap-3">
-            <div className="flex items-center space-x-2">
-                <img className="w-6 h-6" src={discount} alt="image" />
-                <span className="font-semibold text-[#676971] text-2xl">Phiếu ưu đãi</span>
-            </div>
-            <span className="text-3xl">&gt;</span>
+          <div className="flex items-center space-x-2">
+            <img className="w-6 h-6" src={discount} alt="image" />
+            <span className="font-semibold text-[#676971] text-2xl">
+              Phiếu ưu đãi
+            </span>
+          </div>
+          <span className="text-3xl">&gt;</span>
         </div>
 
         <div className="flex items-center justify-between text-sm font-semibold border-b border-gray-300 py-4 m-0">
@@ -140,85 +141,87 @@ const CartPage = () => {
 
           {/* Thông tin nhận hàng */}
           <div className="flex flex-col space-y-2">
-                        <div className="flex items-center space-x-2 mb-4 mt-4">
-                            <img className="w-6 h-6" src={location} alt="image" />
-                            <label
-                                htmlFor="shipping-address"
-                                className="font-semibold text-lg text-[#676971]"
-                            >
-                                Thông tin nhận hàng
-                            </label>
-                        </div>
-                        <span className="text-sm text-black mb-4 font-semibold">Quốc gia: Việt Nam</span>
-
-                        <div className="flex items-center gap-2 font-semibold">
-                          <span className="text-red-500">*</span>
-                          <span className="text-sm text-black mb-1">Địa chỉ</span>
-                        </div>
-                        <input
-                            id="shipping-address"
-                            className="w-full p-2 border border-black rounded-md text-[#676971] text-sm text-center"
-                            placeholder="Số nhà - Tên đường - Thôn/Xã"
-                        />
-                        <div className="flex items-center gap-2 font-semibold">
-                          <span className="text-red-500">*</span>
-                          <span className="text-sm text-black mb-1">Tỉnh/Thành Phố</span>
-                        </div>
-                        <input
-                            id="shipping-city"
-                            className="w-full p-2 border border-black rounded-md text-[#676971] text-sm text-center"
-                            placeholder="Tỉnh/Huyện/Thành phố"
-                        />
-                    </div>
-
-                    {/* Thông tin bổ sung */}
-                    <div className="flex flex-col space-y-2">
-                        <div className="flex items-center space-x-2 mb-3 mt-4">
-                            <label
-                                htmlFor="order-notes"
-                                className="font-semibold text-lg text-[#676971]"
-                            >
-                                THÔNG TIN BỔ SUNG
-                            </label>
-                        </div>
-                        <span className="text-sm text-black mb-1 font-semibold">Yêu cầu khác</span>
-                        <input
-                            id="order-notes"
-                            className="w-full p-2 border border-black rounded-md text-[#676971] text-sm text-center"
-                            placeholder="Nhập yêu cầu ( Không bắt buộc)"
-                        />
-                    </div>
-
-                    {/* Phương thức thanh toán */}
-                    <div className="flex flex-col space-y-2">
-                        <div className="flex items-center space-x-2 mb-4 mt-4">
-                            <img className="w-6 h-6" src={creditcard} alt="image" />
-                            <label
-                                className="font-semibold text-lg text-[#676971]"
-                            >
-                                Phương thức thanh toán
-                            </label>
-                        </div>
-                        <label className="bg-[#F6F6F6] text-black font-semibold rounded-md border border-[#E0E0E0] text-sm flex items-center p-3 cursor-pointer mb-5">
-                            <input
-                                type="radio"
-                                name="paymentMethod" // Thêm name để nhóm các radio button
-                                value="cod" // Thêm value cho radio button
-                                className="w-4 h-4 mr-3 "
-                                checked
-                            />
-                            <span>Thanh Toán Khi Nhận Hàng</span>
-                        </label>
-                    </div>
-
-                    {/* Nút Đặt Hàng */}
-                    <button className="bg-[#A51717] text-white w-full py-3 rounded-full text-sm font-semibold mb-5">
-                        Đặt Hàng
-                    </button>
-                </div>
+            <div className="flex items-center space-x-2 mb-4 mt-4">
+              <img className="w-6 h-6" src={location} alt="image" />
+              <label
+                htmlFor="shipping-address"
+                className="font-semibold text-lg text-[#676971]"
+              >
+                Thông tin nhận hàng
+              </label>
             </div>
+            <span className="text-sm text-black mb-4 font-semibold">
+              Quốc gia: Việt Nam
+            </span>
+
+            <div className="flex items-center gap-2 font-semibold">
+              <span className="text-red-500">*</span>
+              <span className="text-sm text-black mb-1">Địa chỉ</span>
+            </div>
+            <input
+              id="shipping-address"
+              className="w-full p-2 border border-black rounded-md text-[#676971] text-sm text-center"
+              placeholder="Số nhà - Tên đường - Thôn/Xã"
+            />
+            <div className="flex items-center gap-2 font-semibold">
+              <span className="text-red-500">*</span>
+              <span className="text-sm text-black mb-1">Tỉnh/Thành Phố</span>
+            </div>
+            <input
+              id="shipping-city"
+              className="w-full p-2 border border-black rounded-md text-[#676971] text-sm text-center"
+              placeholder="Tỉnh/Huyện/Thành phố"
+            />
+          </div>
+
+          {/* Thông tin bổ sung */}
+          <div className="flex flex-col space-y-2">
+            <div className="flex items-center space-x-2 mb-3 mt-4">
+              <label
+                htmlFor="order-notes"
+                className="font-semibold text-lg text-[#676971]"
+              >
+                THÔNG TIN BỔ SUNG
+              </label>
+            </div>
+            <span className="text-sm text-black mb-1 font-semibold">
+              Yêu cầu khác
+            </span>
+            <input
+              id="order-notes"
+              className="w-full p-2 border border-black rounded-md text-[#676971] text-sm text-center"
+              placeholder="Nhập yêu cầu ( Không bắt buộc)"
+            />
+          </div>
+
+          {/* Phương thức thanh toán */}
+          <div className="flex flex-col space-y-2">
+            <div className="flex items-center space-x-2 mb-4 mt-4">
+              <img className="w-6 h-6" src={creditcard} alt="image" />
+              <label className="font-semibold text-lg text-[#676971]">
+                Phương thức thanh toán
+              </label>
+            </div>
+            <label className="bg-[#F6F6F6] text-black font-semibold rounded-md border border-[#E0E0E0] text-sm flex items-center p-3 cursor-pointer mb-5">
+              <input
+                type="radio"
+                name="paymentMethod" // Thêm name để nhóm các radio button
+                value="cod" // Thêm value cho radio button
+                className="w-4 h-4 mr-3 "
+                checked
+              />
+              <span>Thanh Toán Khi Nhận Hàng</span>
+            </label>
+          </div>
+
+          {/* Nút Đặt Hàng */}
+          <button className="bg-[#A51717] text-white w-full py-3 rounded-full text-sm font-semibold mb-5">
+            Đặt Hàng
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CartPage;
