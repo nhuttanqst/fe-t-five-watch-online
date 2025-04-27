aa56b8c7b50f34e3b19134f986b3bc07322b1730
Ngo Nhut Tan <ngonhattan.sd1@gmail.com>
2025-04-27 23:11:16 +0700

    update

--- src/pages/client/Home.jsx
+++ src/pages/client/Home.jsx
@@ -1,11 +1,8 @@
-
-import { useEffect, useState } from 'react';
-import Banner from '../../components/Banner';
-import FeaturedCategory from '../../components/FeaturedCategory';
-import PopularWatches from '../../components/PopularWatches';
-import ProductCategories from '../../components/ProductCategory';
-import useWatches from '../../apiservice/apiProduct';
-
+import Banner from "../../components/Banner";
+import FeaturedCategory from "../../components/FeaturedCategory";
+import PopularWatches from "../../components/PopularWatches";
+import ProductCategories from "../../components/ProductCategory";
+import useWatches from "../../apiservice/apiProduct";
 
 const SkeletonLoader = () => (
   <div className="grid gap-6 mx-20 grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
@@ -20,10 +17,9 @@ const SkeletonLoader = () => (
 );
 
 const HomePage = () => {
-  const maleWatchesHook = useWatches(1, 4, 'Nam');
-  const femaleWatchesHook = useWatches(1, 4, 'Nữ');
-  const coupleWatchesHook = useWatches(1, 4, 'Couple');
-
+  const maleWatchesHook = useWatches(1, 4, "Nam");
+  const femaleWatchesHook = useWatches(1, 4, "Nữ");
+  const coupleWatchesHook = useWatches(1, 4, "Couple");
 
   return (
     <div className="container mt-4 mb-20 mx-auto">
@@ -35,27 +31,32 @@ const HomePage = () => {
         <SkeletonLoader />
       ) : (
         <>
-          <PopularWatches watches={maleWatchesHook.watches} title="ĐỒNG HỒ NAM MỚI NHẤT" />
-          
+          <PopularWatches
+            watches={maleWatchesHook.watches}
+            title="ĐỒNG HỒ NAM MỚI NHẤT"
+          />
         </>
       )}
       {femaleWatchesHook.loading ? (
         <SkeletonLoader />
       ) : (
         <>
-          <PopularWatches watches={femaleWatchesHook.watches} title="ĐỒNG HỒ NỮ MỚI NHẤT" />
-          
+          <PopularWatches
+            watches={femaleWatchesHook.watches}
+            title="ĐỒNG HỒ NỮ MỚI NHẤT"
+          />
         </>
       )}
       {coupleWatchesHook.loading ? (
         <SkeletonLoader />
       ) : (
         <>
-          <PopularWatches watches={coupleWatchesHook.watches} title="ĐỒNG HỒ CẶP ĐÔI MỚI NHẤT" />
-          
+          <PopularWatches
+            watches={coupleWatchesHook.watches}
+            title="ĐỒNG HỒ CẶP ĐÔI MỚI NHẤT"
+          />
         </>
       )}
-
     </div>
   );
 };
